49ad3c554681792593d0330858b1fb3791b98329
Funkyskep <phyrowolf24@gmail.com>
2024-09-18 13:50:55 -0400

    more things added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let age = 18;
 
 //if age is greater than 20, and less than 50, print out console log "you are young"
 
-
+/*CONDITIONALS*/
 //in one line
 age > 20 && age < 50 ? console.log('you are old') : console.log('you are young');
 
@@ -17,5 +17,28 @@ if (age > 20 && age < 50){
     console.log('you are young')
 }
 
+/*ARRAYS*/ 
+
+const fruit = ['apples', 'orange', 'kiwi']
+
+console.log(fruit[1])
+
+
 
 console.log('test');
+
+let data = ['person', 44, 'place']
+
+console.log(data[1])
+
+data[1]++;
+
+console.log(data[1])
+
+// loops
+
+for (let count = 0; count < 11; count++) {
+    console.log(count);
+    
+}
+let array = [at1,at4]
